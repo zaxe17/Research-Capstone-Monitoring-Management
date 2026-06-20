189601696bfb76e0ad48718e24d1be6550e973bc
zaxe17 <jmjacolbiapogi17@gmail.com>
2026-06-20 17:15:03 +0800

    admin and fixed Db

--- obj/Debug/net10.0/monitoring_management.AssemblyInfo.cs
+++ obj/Debug/net10.0/monitoring_management.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("monitoring_management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae084a594c4ec53f35e5571290250bd25811d60f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e84e62ac29f011de56122c58923daa30714cb086")]
 [assembly: System.Reflection.AssemblyProductAttribute("monitoring_management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("monitoring_management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
